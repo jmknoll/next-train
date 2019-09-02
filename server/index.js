bcb0b5b797328052ee6f970c0f35a9ab6ecaf449
Jameson Knoll <j.knoll621@gmail.com>
2019-09-02 15:00:37 -0400

    update sever loggiong

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
+  response.json({db})
 })
 
 app.get('/routes', db.getRoutes);
