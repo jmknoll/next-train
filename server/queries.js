fd78f3632c7b11f6057a68587b601141cb8e83c0
Jameson Knoll <j.knoll621@gmail.com>
2019-09-02 12:53:07 -0400

    refactor requests

--- server/queries.js
+++ server/queries.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'mta_api',
-  password: 'password',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 })
 
 var moment = require('moment')
