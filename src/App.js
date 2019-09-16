a50ff07145eaba8c07b8394e98d835b7c0d58d33
Jameson Knoll <j.knoll621@gmail.com>
2019-09-16 00:33:34 -0400

    add stop selection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import DataService from "./dataService";
+import Box from "@material-ui/core/Box";
+import Header from "./AppBar";
+import Card from "./Card";
+import Select from "react-select";
+
 const dataService = new DataService();
 
 const transformLineNumber = val => {
@@ -11,26 +14,67 @@ const transformLineNumber = val => {
   return lineNumberMap[val];
 };
 
-function App() {
-  const data = dataService.getData(transformLineNumber("f"));
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      trains: [],
+      stops: []
+    };
+  }
+
+  fetchData() {
+    dataService
+      .getData({ time: Date.now(), station: "236", direction: "N" })
+      .then(trains => {
+        this.setState({ trains });
+      });
+  }
+
+  fetchRoutes() {
+    dataService.getRoutes().then(routes => {
+      this.setState({ routes });
+    });
+  }
+
+  fetchStops() {
+    dataService.getStops().then(stops => {
+      console.log(stops);
+      this.setState({ stops });
+    });
+  }
+
+  componentDidMount() {
+    this.fetchData();
+    this.fetchRoutes();
+    this.fetchStops();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Select
+          className="basic-single"
+          classNamePrefix="select"
+          defaultValue={""}
+          isDisabled={false}
+          isLoading={false}
+          isClearable={true}
+          isRtl={false}
+          isSearchable={true}
+          name="color"
+          options={this.state.stops.map(stop => {
+            return { value: stop.stop_id, label: stop.stop_name };
+          })}
+        />
+        {this.state.trains.map(train => {
+          return <Card train={train} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/dataService.js
+++ src/dataService.js
@@ -1,37 +1,60 @@
-import axios from 'axios';
+import axios from "axios";
+import { resolve } from "url";
 
 const { REACT_APP_MTA_BASE_URL, REACT_APP_MTA_API_KEY } = process.env;
 
-
-
 class DataService {
+  constructor() {}
 
-  constructor() {
-  
+  getData({ time, station, direction }) {
+    return axios
+      .get(
+        `http://localhost:3000/nextTrain?time=${time}&station=236&direction=N`
+      )
+
+      .then(response => {
+        return response.data;
+      })
+      .catch(function(error) {
+        // handle error
+        console.log("err", error);
+      })
+      .finally(function(res) {
+        console.log("finally", res);
+        // always executed
+      });
   }
 
-  getData(val) {
-    const requestUrl = `${REACT_APP_MTA_BASE_URL}?key=${REACT_APP_MTA_API_KEY}&feed_id=${val}`
-    axios({
-      method: 'POST',
-      url: 'http://localhost:8888/.netlify/functions/provideTransitData',
-      data: {
-        requestUrl
-      },
-      responseType: 'arraybuffer',
-    })
-    .then(function (response) {
-      // handle success
-      console.log('res', response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log('err', error);
-    })
-    .finally(function (res) {
-      console.log('finally', res)
-      // always executed
-    });
+  getRoutes() {
+    return axios
+      .get(`http://localhost:3000/routes`)
+      .then(response => {
+        return response.data;
+      })
+      .catch(function(error) {
+        // handle error
+        console.log("err", error);
+      })
+      .finally(function(res) {
+        console.log("finally", res);
+        // always executed
+      });
+  }
+
+  getStops() {
+    return axios
+      .get(`http://localhost:3000/stops`)
+      .then(response => {
+        return response.data;
+      })
+      .catch(function(error) {
+        // handle error
+        console.log("err", error);
+      })
+      .finally(function(res) {
+        console.log("finally", res);
+        // always executed
+      });
   }
 }
 
